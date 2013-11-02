dfdc4fa92729171c22c7f2ea3f430ec4de9060cd
esjee <ewfawefw3@gmail.com>
2013-11-02 17:35:27 +0100

    Put config in repo

--- config.h
+++ config.h
@@ -1,93 +1,135 @@
 /* appearance */
-static const char font[]            = "-misc-terminusmodx-*-*-normal-*-12-120-72-72-c-60-iso10646-1";
-//static const char font[]            = "-*-terminusmodx-medium-r-normal-*-12-*-*-*-*-*-*-*";
+static const char font[]            = "terminusmodx 8";
+
+/* normal */
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#cccaca";
+
+/* selected */
+static const char selbordercolor[]  = "#390c5c";
+static const char selbgcolor[]      = "#161616";
+static const char selfgcolor[]      = "#eeecec";
+
+/* backup
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
+backup */
+
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
-
-#define FIREFOX_GTKFIX "env GTK2_RC_FILES=/home/plague/.static/firefox-gtkrc firefox"
+static const Bool topbar            = False;     /* False means bottom bar */
 
 /* tagging */
-//static const char *tags[] = {"壱", "弐", "参", "肆", "伍", "陸", "漆", "捌", "玖"/*, "拾"*/ };
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] =
+	{"壱", "弐", "参", "肆", "伍", "陸", "漆", "捌", "玖"/*, "拾"*/ };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 8,            True,        -1 },
-	//{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+/* class instance title tags mask isfloating monitor */
+{ "Gimp",	NULL,	NULL,	1 << 8,	True,	-1 },
+//{ "Firefox",  NULL,	NULL,	1 << 8,	False,	-1 },
 };
 
-/* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+/* layout */
+/* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.64;
+/* number of clients in master area */
+static const int nmaster      = 1;
+/* True means respect size hints in tiled resizals */
+static const Bool resizehints = True;
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile },    /* first entry is default */
-	{ "[F]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "普通",	tile },    /* first entry is default */
+	{ "自由",	NULL },    /* NULL means floating behavior */
+	{ "大",		monocle },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,			KEY, view,	{.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,		KEY, toggleview,{.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,		KEY, tag,	{.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY, toggletag,	{.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+#define FIREFOX_GTKFIX ""\
+	"env GTK2_RC_FILES=/home/plague/.static/firefox-gtkrc firefox"
+#define VOLUME_UP "amixer set Master 3%+"
+#define VOLUME_DOWN "amixer set Master 3%-"
+#define MUSIC_TOGGLE "mpc -q toggle"
+#define MUSIC_NEXT "mpc -q next"
+#define MUSIC_PREV "mpc -q prev"
+
+static const char *dmenucmd[] =
+	{"dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor,
+		"-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "/usr/bin/urxvtc", NULL };
-static const char *browser[]  = { "env GTK2_RC_FILES=/home/plague/.static/firefox-gtkrc firefox", NULL };
 
 static Key keys[] = {
+	/* modifiers	keys	functions	argument */
+	{ MODKEY,		XK_r,		spawn,		{.v = dmenucmd } },
+	{ MODKEY,		XK_Return,	spawn,		{.v = termcmd } },
+	/* window manipulation */
+	{ MODKEY,		XK_b,		togglebar,	{0} },
+	{ MODKEY,		XK_j,		focusstack,	{.i = +1 } },
+	{ MODKEY,		XK_k,		focusstack,	{.i = -1 } },
+	{ MODKEY,		XK_i,		incnmaster,	{.i = +1 } },
+	{ MODKEY,		XK_d,		incnmaster,	{.i = -1 } },
+	{ MODKEY,		XK_h,		setmfact,	{.f = -0.05} },
+	{ MODKEY,		XK_l,		setmfact,	{.f = +0.05} },
+	{ MODKEY|ShiftMask,	XK_j,		pushdown,	{0} },
+	{ MODKEY|ShiftMask,	XK_k,		pushup,		{0} },
+	{ MODKEY,		XK_f,		zoom,		{0} },
+	{ MODKEY,		XK_Tab,		view,		{0} },
+	{ MODKEY|ShiftMask,	XK_c,		killclient,	{0} },
+	{ MODKEY,		XK_t,		setlayout,	{.v = &layouts[0]} },
+	//{ MODKEY,XK_f,ssetlayout,s{.v = &layouts[1]} },
+	{ MODKEY,		XK_m,		setlayout,	{.v = &layouts[2]} },
+	{ MODKEY,		XK_space,	setlayout,	{0} },
+	{ MODKEY|ShiftMask,	XK_space,	togglefloating,	{0} },
+	{ MODKEY,		XK_0,		view,		{.ui = ~0 } },
+	{ MODKEY|ShiftMask,	XK_0,		tag,		{.ui = ~0 } },
+	{ MODKEY,		XK_comma,	focusmon,	{.i = -1 } },
+	{ MODKEY,		XK_period,	focusmon,	{.i = +1 } },
+	{ MODKEY|ShiftMask,	XK_comma,	tagmon,		{.i = -1 } },
+	{ MODKEY|ShiftMask,	XK_period,	tagmon,		{.i = +1 } },
+
+	/* Tag jumping */
+	TAGKEYS(XK_1,0)
+	TAGKEYS(XK_2,1)
+	TAGKEYS(XK_3,2)
+	TAGKEYS(XK_4,3)
+	TAGKEYS(XK_5,4)
+	TAGKEYS(XK_6,5)
+	TAGKEYS(XK_7,6)
+	TAGKEYS(XK_8,7)
+	TAGKEYS(XK_9,8)
+
+	/* dwm commands */
+	{ MODKEY|ShiftMask,XK_q,quit,{0} },
+
+	/* applications */
+	{ MODKEY,XK_s,spawn,SHCMD(FIREFOX_GTKFIX) },
+
+	/* volume, music */
+	{ MODKEY,XK_equal,spawn,SHCMD(VOLUME_UP) },
+	{ MODKEY,XK_minus,spawn,SHCMD(VOLUME_DOWN) },
+	{ MODKEY,XK_p,spawn,SHCMD(MUSIC_TOGGLE) },
+	{ MODKEY,XK_bracketright,spawn,SHCMD(MUSIC_NEXT) },
+	{ MODKEY,XK_bracketleft,spawn,SHCMD(MUSIC_PREV) }
+
 	/* modifier                     key        function        argument */
-	{ MODKEY,             XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,             XK_s,      spawn,           SHCMD(FIREFOX_GTKFIX) },
 };
 
 /* button definitions */
@@ -108,3 +150,4 @@ static Button buttons[] = {
 };
 
 /* vim: set ft=c: */
+/* vim: set cc=0: */
