8ad761ce636761fd6055524dd6d396690a43af2d
esjee <ewfawefw3@gmail.com>
2013-11-02 15:22:15 +0100

    let gen-patch generate patch based on branch name

--- gen-patch.sh
+++ gen-patch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 GIT="/usr/bin/git"
+
+CBRANCH="$(git rev-parse --abbrev-ref HEAD)"
+#OUT="99-$(echo "$CBRANCH").patch"
 OUT="99-esjee.patch"
 
-"$GIT" diff master esjee >"$OUT"
+"$GIT" diff master "$CBRANCH" >"$OUT"
