c835f23d29c164918c6ac20e297e28e3375290a9
esjee <ewfawefw3@gmail.com>
2013-11-02 22:59:18 +0100

    fix gen-patch.sh

--- gen-patch.sh
+++ gen-patch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 GIT="/usr/bin/git"
-
+WHITELIST="config.def.h config.mk dwm.1 dwm.c LICENSE Makefile README"
 CBRANCH="$(git rev-parse --abbrev-ref HEAD)"
 #OUT="99-$(echo "$CBRANCH").patch"
 OUT="99-esjee.patch"
 
-"$GIT" diff master "$CBRANCH" >"$OUT"
+$GIT diff master $CBRANCH -- $WHITELIST >"$OUT"
