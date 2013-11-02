321bd8b41f417b960437d80dc8caed82fde41129
esjee <ewfawefw3@gmail.com>
2013-11-02 13:58:40 +0100

    Apply pango (w/ &r fix)

--- config.mk
+++ config.mk
@@ -14,9 +14,13 @@ X11LIB = /usr/lib
 XINERAMALIBS = -L${X11LIB} -lXinerama
 XINERAMAFLAGS = -DXINERAMA
 
+# Pango XFT
+PANGOINC = -pthread -I/usr/include/pango-1.0 -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -I/usr/include/freetype2
+PANGOLIB = -lpangoxft-1.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfreetype -lfontconfig -lXft
+
 # includes and libs
-INCS = -I.  -I${X11INC}
-LIBS =  -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
+INCS = -I.  -I${X11INC} ${PANGOINC}
+LIBS =  -lc -L${X11LIB} -lX11 ${XINERAMALIBS} ${PANGOLIB}
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
