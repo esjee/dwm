a74feb973785fe58408ce0a9248dff9226765d6e
esjee <ewfawefw3@gmail.com>
2013-11-07 23:41:19 +0100

    clean up gen-patch.sh

--- gen-patch.sh
+++ gen-patch.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-GIT="/usr/bin/git"
 WHITELIST="config.def.h config.mk dwm.1 dwm.c LICENSE Makefile README mpdcontrol.c"
 CBRANCH="$(git rev-parse --abbrev-ref HEAD)"
 #OUT="99-$(echo "$CBRANCH").patch"
 OUT="99-esjee.patch"
 
-$GIT diff master $CBRANCH -- $WHITELIST >"$OUT"
+git diff master $CBRANCH -- $WHITELIST >"$OUT"
