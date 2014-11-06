c41898f80172e87e7cf942b019920b9eb828bc90
esjee <ewfawefw3@gmail.com>
2014-11-07 06:42:59 +0900

    ugly workaround b/ bug

--- gen-patch.sh
+++ gen-patch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 CBRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
-for i in config.def.h config.mk dwm.1 dwm.c LICENSE Makefile README mpdcontrol.c
+for i in config.def.h config.mk dwm.1 dwm.c LICENSE Makefile README
 do
 	OUT="99-esjee-${i}.patch"
 	git diff master $CBRANCH -- ${i} >"$OUT"
+done
+for i in  mpdcontrol.c
+do
+	OUT="99-esjee-${i}.patch"
+	git diff master $CBRANCH -- ${i} >"$OUT"
+	sed -i -e "s_b/${i}_${i}_g" "$OUT"
 done
 make clean >/dev/null
